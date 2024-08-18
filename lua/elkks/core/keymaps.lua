7eaf1b0047e21305fd98e3b078b38e247a0b2a7b
elkks19 <rafafabiani1909@gmail.com>
2024-08-18 00:50:44 -0400

    use <leader>gf to find hidden files

--- lua/elkks/core/keymaps.lua
+++ lua/elkks/core/keymaps.lua
@@ -25,7 +25,7 @@ keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
-keymap.set("n", "<leader>gf", "<cmd>Telescope git_files<cr>")
+keymap.set("n", "<leader>gf", "<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files,-u<cr>")
 
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
--- lua/elkks/plugins/telescope.lua
+++ lua/elkks/plugins/telescope.lua
@@ -1,43 +1,42 @@
 local telescope_setup, telescope = pcall(require, "telescope")
 if not telescope_setup then
-   return
+	return
 end
 
 local actions_setup, actions = pcall(require,  "telescope.actions")
 if not actions_setup then
-   return 
+	return
 end
 
 
 telescope.setup({
-   defaults = {
-      layout_config = {
-         horizontal = {
-         preview_cutoff = 0,
-         },
-      },
-      mappings = {
-         i = {
-            ["<C-k>"] = actions.move_selection_previous,
-            ["<C-j>"] = actions.move_selection_next,
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-         },
-      },
-	  extensions = {
-		  ["ui-select"] = {
-			  require("telescope.themes").get_dropdown {
+	defaults = {
+		layout_config = {
+			horizontal = {
+				preview_cutoff = 0,
+			},
+		},
+		mappings = {
+			i = {
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-j>"] = actions.move_selection_next,
+				["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+			},
+		},
+		extensions = {
+			["ui-select"] = {
+				require("telescope.themes").get_dropdown {
 
-			  }
-		  },
-
-		  emoji = {
-			  action = function(emoji)
-				  vim.fn.setreg("", emoji.value)
-				  print([[Presiona p o "*p para pegar este emoji]] .. emoji.value)
-			  end,
-		  }
-	  },
-   },
+				}
+			},
+			emoji = {
+				action = function(emoji)
+					vim.fn.setreg("", emoji.value)
+					print([[Presiona p o "*p para pegar este emoji]] .. emoji.value)
+				end,
+			}
+		},
+	},
 })
 
 telescope.load_extension("fzf")
