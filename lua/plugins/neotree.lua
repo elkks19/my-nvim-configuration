fe6ff24b4606e6018b9721e684b7b376edc71672
elkks19 <rafafabiani1909@gmail.com>
2024-10-27 17:38:43 -0400

    added a command to neotree so it opens zathura on pdf

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -14,9 +14,30 @@ return {
         window = {
 			width = 30,
             mappings = {
-                ["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true }}
+                ["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true }},
+				["O"] = "system_open"
             },
         },
+		commands = {
+			system_open = function(state)
+				local node = state.tree:get_node()
+				local path = node:get_id()
+				-- macOs: open file in default application in the background.
+				vim.fn.jobstart({ "xdg-open", "-g", path }, { detach = true })
+				-- Linux: open file in default application
+				vim.fn.jobstart({ "xdg-open", path }, { detach = true })
+
+				-- Windows: Without removing the file from the path, it opens in code.exe instead of explorer.exe
+				local p
+				local lastSlashIndex = path:match("^.+()\\[^\\]*$") -- Match the last slash and everything before it
+				if lastSlashIndex then
+					p = path:sub(1, lastSlashIndex - 1) -- Extract substring before the last slash
+				else
+					p = path -- If no slash found, return original path
+				end
+  				vim.cmd("silent !start explorer " .. p)
+			end
+		},
         close_if_last_window = true,
   		auto_clean_after_session_restore = true,
 		filesystem = {
@@ -41,4 +62,3 @@ return {
 		},
     },
 }
-
